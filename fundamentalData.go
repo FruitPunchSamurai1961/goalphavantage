376b1af40efc7cb43a1757cf38b2e9d62b3e49f7
FruitPunchSamurai <spatel752@gatech.edu>
2024-01-01 17:32:10 -0500

    Change GetLatestActiveListingStatus to allow getting delisted and past Active/Non-Active Listing status
    Changes:
    Added ListingStatusOptions which is used by the GetListingStatus method and passed to the api call.
    
    Added validation checks
    
    Added more checks in tests and refactored repeated code

--- fundamentalData.go
+++ fundamentalData.go
@@ -4,22 +4,70 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"regexp"
+	"strings"
+	"time"
 )
 
-type ActiveListing struct {
+type State string
+
+func (s State) Valid() bool {
+	switch strings.ToLower(string(s)) {
+	case "", "active", "delisted":
+		return true
+	default:
+		return false
+	}
+}
+
+func (l ListingStatusOptions) Valid() bool {
+	if !l.State.Valid() {
+		return false
+	}
+
+	if l.Date != "" {
+		//Check for YYYY-MM-DD
+		dateRegex := regexp.MustCompile(`^\d{4}-\d{2}-\d{2}$`)
+		if !dateRegex.MatchString(l.Date) {
+			return false
+		}
+
+		dateValue, err := time.Parse("2006-01-02", l.Date)
+		if err != nil {
+			return false
+		}
+
+		minimumDate := time.Date(2010, 1, 1, 0, 0, 0, 0, time.UTC)
+		if dateValue.Before(minimumDate) {
+			return false
+		}
+	}
+
+	return true
+}
+
+type ListingStatusOptions struct {
+	Date  string `url:"date"`
+	State State  `url:"state"`
+}
+
+type Listing struct {
 	Symbol    string `json:"symbol"`
 	Name      string `json:"name"`
 	Exchange  string `json:"exchange"`
 	AssetType string `json:"assetType"`
 }
 
-func (c *Client) GetLatestActiveListingStatus(ctx context.Context) (*[]ActiveListing, error) {
-	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%sfunction=LISTING_STATUS", c.BaseURL), nil)
+func (c *Client) GetListingStatus(ctx context.Context, options *ListingStatusOptions) (*[]Listing, error) {
+	if options != nil && !options.Valid() {
+		return nil, InValidInputError
+	}
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%sfunction=LISTING_STATUS&%s", c.BaseURL, c.buildQuery(options)), nil)
 	if err != nil {
 		return nil, err
 	}
 	req = req.WithContext(ctx)
-	var res []ActiveListing
+	var res []Listing
 	if err = c.doCSVRequest(req, &res); err != nil {
 		return nil, fmt.Errorf("failed to get latest active listing status: %w", err)
 	}
